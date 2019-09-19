b2ff9a1d09820a3a8072797d7fab7e372815ad36
60015023Tanaporn <54892005+60015023Tanaporn@users.noreply.github.com>
2019-09-19 13:11:23 +0700

    อ่านค่าความชื้นในดินเพื่อควบคุมปั๊มน้ำ

---
+++ soil_Moistur/soil_Moistur.ino
@@ -0,0 +1,18 @@
+int pin_pump = 15;
+void setup() {
+   Serial.begin(9600);
+   pinMode(pin_pump,OUTPUT);
+}
+
+void loop() {
+  int val_moistur = analogRead(A0);//อ่านค่าความชื้นในดินจากA0
+  Serial.println(val_moistur);
+  delay(1000);
+  if (val_moistur<500){ //ถ้าค่าความชื้นในดินน้อยกว่า500ให้ปั๊มน้ำทำงาน
+    digitalWrite(pin_pump,HIGH);
+  }
+  else{
+    digitalWrite(pin_pump,LOW);
+  }
+
+}
