f65c12f3ea08f04f4a3b5ea3a621c4a9e19f2af1
Tanaporn <54892005+60015023Tanaporn@users.noreply.github.com>
2019-11-07 13:30:40 +0700

    Update mainmenu.ino

--- mainmenu/mainmenu.ino
+++ mainmenu/mainmenu.ino
@@ -1,6 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>                   // เรียก Library
+#include <LiquidCrystal_I2C.h>      // เรียก Library LCD
+#include <DHT.h>                  // เรียก Library DHT
+ 
+#define DHTPIN D3        // DHT Pin  กำหนดเป็น D3
+#define DHTTYPE DHT22   // เรียกใช้DHT 22
+int cooling_pin =2; //ประกาศตัวแปร cooling_pin 2 = ขาD4 = GPIO2
+              
 
+DHT dht(DHTPIN, DHTTYPE);   // ประกาศตัวแปรคลาส dht
+ 
+LiquidCrystal_I2C lcd(0x27, 16, 2); //ประกาศตัวแปร LiquidCrystal_I2C LCD 16x2
+void setup() {
+  Serial.begin(9600);   
+  dht.begin();            // initial ตัวแปรคลาส dht
+  lcd.begin();            // initial ตัวแปรคลาส lcd
+  lcd.backlight();        // เปิดไฟ backlight
 }
 
 void loop() {
