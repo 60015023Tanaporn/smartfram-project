34ee4a77aaecc799a2c354f3d7eb116283a0e3bf
Tanaporn <54892005+60015023Tanaporn@users.noreply.github.com>
2019-11-14 15:53:37 +0700

    Update mainmenu.ino

--- mainmenu/mainmenu.ino
+++ mainmenu/mainmenu.ino
@@ -18,6 +18,25 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("brightness =");
+  Serial.print(ledmap); //โชว์ค่าออกมาเป็น%
+  Serial.print("%\n");
+  ledmap = map(brightness,0,255,0,99); //เทียบค่าpwm(0-255)ออกมาเป็น%(0-100)
+  analogWrite(pin_led,brightness); // output เป็นpwm ใช้ได้ทุกขายกเว้น D0
+  
+  if(digitalRead(button_up)==LOW){ //ถ้าสถานะปุ่มกดUPเท่ากับLOW(ต่อแบบpull up)แล้วbrightnessน้อยกว่า255
+    if (brightness<255){ /
+      brightness++,brightness+=12,brightness==255;//ให้brightnessเพิ่มได้ทีละ12 จนเท่ากัมากกว่า255
+      delay(150);
+    }
+  }
+  
+  if(digitalRead(button_down)==LOW){//ถ้าสถานะปุ่มกดdown เท่ากับLOW(ต่อแบบpullup)แล้วbrightnessมากกกว่า0
+    if (brightness>0){
+      brightness--,brightness-=12,brightness==0;//ให้brightnessลดลงได้ทีละ12 จนเหลือ0
+      delay(150);
+    }
+  }
+  delay(100);
 
 }
