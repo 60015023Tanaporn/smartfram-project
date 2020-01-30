0078fa5154807bba959d6913f3e5da04eab31504
Tanaporn <54892005+60015023Tanaporn@users.noreply.github.com>
2020-01-30 17:49:55 +0700

    Update database.sql

--- smartfarmpi/smartfarmpi-php/images/images/database/database.sql
+++ smartfarmpi/smartfarmpi-php/images/images/database/database.sql
@@ -10,6 +10,14 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 19, 2019 at 05:12 AM
+-- Server version: 5.7.20-0ubuntu0.17.04.1
+-- PHP Version: 5.5.9-1ubuntu4.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
