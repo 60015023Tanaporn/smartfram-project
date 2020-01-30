93e32b6ace4aef5630617ce80727c1fbce3926fe
Tanaporn <54892005+60015023Tanaporn@users.noreply.github.com>
2020-01-30 17:48:03 +0700

    Update hw_aircon.py

--- smartfarmpi/smartfarmpi-py/hw_aircon.py
+++ smartfarmpi/smartfarmpi-py/hw_aircon.py
@@ -6,3 +6,13 @@ def set_aircon_value(value):
 
 def set_temp_value(value):
     pass
+
+
+#import hw_power
+
+#def set_aircon_value(value):
+ #   hw_power.set_aircon_value(value)
+
+
+#def set_temp_value(value):
+ #   pass
